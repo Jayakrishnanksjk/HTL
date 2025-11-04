dfe7719e61b85e7f453550c1e9e0f432bc753c83
jayakrishnan <jayakrishnanashi@gmail.com>
2025-11-04 17:32:12 +0530

    news content update in progress

---
+++ js/news.js
@@ -0,0 +1,14 @@
+    // news.js
+    const newsData = [
+    {
+        headline: "HTL Aircon eyes SME listing",
+        publication: "The Hindu",
+        date: "November 25, 2017",
+        image: "images/news/htl-aircon.webp",
+        articleLink: "#",
+        content: `
+        <p>HTL Aircon Pvt. Ltd., a leading HVAC (Heating, Ventilation and Air Conditioning) contractor for commercial and office buildings and a leading dealer for Daikin in India, is planning for a listing on an SME platform to raise resources, a top executive said.</p>
+        <p>The AC engineering company also plans to bid for large contracts as part of its expansion plans.</p>
+        `,
+    },
+    ];
